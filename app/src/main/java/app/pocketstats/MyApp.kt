4a2ac2487aa8a43943f367df4572e007069aae1c
Grant Hoffecker <ulyssesdouglass7@gmail.com>
2022-09-14 08:49:08 -0700

    make logo colorful, add note to tutorial, make stats card

--- app/src/main/java/app/pocketstats/MyApp.kt
+++ app/src/main/java/app/pocketstats/MyApp.kt
@@ -1,6 +1,10 @@
 package app.pocketstats
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -8,6 +12,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,9 +35,13 @@ fun MyApp(dateModel: DataViewModel = viewModel()) {
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
             ) {
                 Logo()
+                Divider()
+                Spacer(modifier = Modifier.height(16.dp))
                 Tutorial()
                 Spacer(modifier = Modifier.height(16.dp))
                 Divider()
@@ -54,12 +63,22 @@ fun MyApp(dateModel: DataViewModel = viewModel()) {
 
 @Composable
 fun Logo() {
-    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()) {
-        Icon(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = null
-        )
-        Text("Pocket Stats", fontSize = 40.sp)
+    Row(
+        verticalAlignment = Alignment.CenterVertically, modifier = Modifier
+//        .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Card(
+            modifier = Modifier.size(80.dp),
+            shape = CircleShape,
+        ) {
+            Image(
+                painter = painterResource(id = R.mipmap.ic_launcher),
+                contentDescription = null,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+        Text("Pocket Stats", fontSize = 40.sp, modifier = Modifier.padding(horizontal = 8.dp))
     }
 }
 
@@ -74,13 +93,14 @@ fun Tutorial() {
             Text("2 Pointer Missed -> Press Volume Down", fontSize = s)
             Text("3 Pointer Made -> Press Volume Up Twice", fontSize = s)
             Text("3 Pointer Missed -> Press Volume Down Twice", fontSize = s)
+            Text("The screen will stay on until you exit the app", fontSize = s)
         }
     }
 }
 
 @Composable
 fun Stats(ups: Int, downs: Int, ups2: Int, downs2: Int) {
-    Column(horizontalAlignment = Alignment.Start) {
+    Column(horizontalAlignment = Alignment.Start, modifier = Modifier.fillMaxWidth()) {
         StatLine(ups, downs, "TWOS MADE")
         StatLine(ups2, downs2, "THREES MADE")
         StatLine(ups + ups2, downs + downs2, "FIELD GOALS")
@@ -93,12 +113,18 @@ fun StatLine(success: Int, failure: Int, metric: String) {
     val percent =
         if (success > 0 || failure > 0) success.toDouble().div(success.plus(failure)).times(100)
             .roundToInt() else 0
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Text(success.toString(), fontSize = 70.sp)
-        Spacer(modifier = Modifier.width(32.dp))
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text("$success / $total = $percent%", fontSize = 14.sp)
-            Text(metric, fontSize = 30.sp)
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 16.dp)) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(success.toString(), fontSize = 70.sp, modifier = Modifier.padding(start = 24.dp))
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("$success / $total = $percent%", fontSize = 14.sp)
+                Text(metric, fontSize = 30.sp, fontStyle = FontStyle.Italic)
+            }
         }
     }
     Spacer(modifier = Modifier.height(32.dp))
